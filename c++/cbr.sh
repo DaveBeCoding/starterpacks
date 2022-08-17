954930e78d0b769e10b70d8ca45db549f1fc3b72
DaveBeCoding <70411835+DaveBeCoding@users.noreply.github.com>
2022-08-17 15:33:28 -0600

    Add files via upload

---
+++ c++/cbr.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+cd autocompile
+./configure.sh; ./build.sh; ./run.sh;
---
+++ c++/main.cxx
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "externals/std_math/test.hpp"
+
+int main(int argc, char const *argv[])
+{
+    int foo = some_really_hard_math(10,5);
+
+    std::cout << "Sampel project -> Plug & Play -> update 00ß" << std::endl;
+    std::cout << foo << "\n";
+    return 0;
+}
